2daea1c6fa731cf4672e3e1a8062c2e57f5deadf
Pedro Casaubon <xperiments@gmail.com>
2025-04-01 17:31:05 +0200

    Update Social Media ddescription

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,8 +45,8 @@ export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
+      description="TigerTag. Future is now."
     >
       <HomepageHeader />
       <main>
