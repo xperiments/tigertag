83f359e92363488f006c9e0ffe388d12bc31185e
Pedro Casaubon <xperiments@gmail.com>
2025-04-01 16:58:26 +0200

    Fix paths

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: "img/favicon.webp",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://xperiments.in",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/tigertag",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
